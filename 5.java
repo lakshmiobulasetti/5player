cbd17f0aa58cdca5be35ab0e069165e4b3e276ec
lakshmiobulasetti <41731019+lakshmiobulasetti@users.noreply.github.com>
2018-09-11 10:50:47 +0530

    Update and rename README.md to 5.java

---
+++ 5.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+     public static void main (String[] args) throws java.lang.Exception
+     {
+     Scanner sc=new Scanner(System.in);
+     String s1=sc.next();
+     String s[]={" ","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"};
+     for(int i=0;i<s.length;i++)
+     {
+     if(s1.equals(s[i]))
+     {
+     System.out.print(i);
+}
+}
+}
+}
